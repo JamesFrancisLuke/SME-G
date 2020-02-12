6468486a71a50d3724dc08c3c51e5446cfe5ce01
James <9418349+JamesFrancisLuke@users.noreply.github.com>
2020-02-12 02:23:00 +0000

    Intial Menu Components
    A prefab for background components on the menu has been produced.

--- SME-G/Assets/Scripts/backgroundElement.cs
+++ SME-G/Assets/Scripts/backgroundElement.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class backgroundElement : MonoBehaviour {
-
+    public float rotationSpeed = 10;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +11,6 @@ public class backgroundElement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(0, 0, 20 * Time.deltaTime);
+        transform.Rotate(0, 0, rotationSpeed * Time.deltaTime);
     }
 }
