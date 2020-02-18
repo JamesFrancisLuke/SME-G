8aab2472cd25338f008bcfb35824a7f8275a23be
Annabel <psyak9@exmail.nottingham.ac.uk>
2020-02-18 18:12:31 +0000

    Timer added to game

---
+++ SME-G/catchfruit/Assets/Script/Timer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+// Annabel
+public class Timer : MonoBehaviour
+{
+    public float timeStart = 60;
+    public Text TimerText; 
+    
+    void Awake(){
+        // find element in UI
+        TimerText = GameObject.Find("TimerText").GetComponent<Text>();
+        TimerText.text = "60";
+    }
+
+	// Use this for initialization
+	void Start () {
+        TimerText.text = timeStart.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeStart -= Time.deltaTime;
+        TimerText.text = Mathf.Round(timeStart).ToString();
+
+        if (timeStart <= 0){
+            // display end of game menu
+        }
+	}
+}
+
+
