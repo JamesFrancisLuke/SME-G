5bdaf40165ac79b9a82ae2409f4c47f22ab1492b
James <9418349+JamesFrancisLuke@users.noreply.github.com>
2020-02-19 07:53:06 +0000

    Implemented Racing Layout
    Racing layout has been added.
    Controls are implemented.
    
    Co-Authored-By: hau5ratz <hau5ratz@users.noreply.github.com>

---
+++ SME-G/Assets/Scripts/playerCar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerCar : MonoBehaviour
+{
+    [SerializeField] float acceleration = 8;
+    [SerializeField] float turnSpeed = 5;
+    Quaternion targetRotation;
+    Rigidbody _rigidBody;
+
+    void Start()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        SetRotationPoint();
+        
+    }
+
+    private void SetRotationPoint()
+    {
+        //Create a Ray
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Plane plane = new Plane(Vector3.up, Vector3.zero);
+        float distance;
+        if(plane.Raycast(ray, out distance))
+        {
+            Vector3 target = ray.GetPoint(distance);
+            Vector3 direction = target - transform.position;
+            float rotationAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg; //Rotate in 2D
+            targetRotation = Quaternion.Euler(0, rotationAngle, 0);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float accelerationInput = acceleration * (Input.GetMouseButton(0) ? 1 : Input.GetMouseButton(1) ? -1 : 0) * Time.fixedDeltaTime;
+        _rigidBody.AddRelativeForce(Vector3.forward * accelerationInput);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSpeed * Time.fixedDeltaTime);
+    }
+}
