ef9173e7aef4fbaba6aca6fc580c252ff5a465bf
Annabel <psyak9@exmail.nottingham.ac.uk>
2020-02-20 21:52:02 +0000

    Linked "Select a Game" and "How to Play" together

--- SME-G/HowToPlay/Assets/Scenes/SelectAGame.cs
+++ SME-G/HowToPlay/Assets/Scenes/SelectAGame.cs
@@ -19,10 +19,7 @@ public class SelectAGame : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    void Update(){}
 
     void formatButtonText(){
         // set text
@@ -31,45 +28,41 @@ public class SelectAGame : MonoBehaviour
         GameObject.Find("BalancingButton").GetComponentInChildren<Text>().text = "Balance the Block";
         GameObject.Find("DodgeButton").GetComponentInChildren<Text>().text = "Dodge the Dog";
 
-        add event listener to buttons
-        Button btn1 = fruit.GetComponent<Button>();
-		btn1.onClick.AddListener(() => onClick(1));
+        // add event listener to buttons
+        fruit = GameObject.Find("CatchFruitButton").GetComponentInChildren<Button>();
+		fruit.onClick.AddListener(()=> switchScenes("1"));
 
-        Button btn2 = racing.GetComponent<Button>(); 
-		btn2.onClick.AddListener(() => onClick(2));
-
-        Button btn3 = balancing.GetComponent<Button>();
-		btn3.onClick.AddListener(() => onClick(3));
-
-        Button btn4 = dodge.GetComponent<Button>();
-		btn4.onClick.AddListener(() => onClick(4));
+        racing = GameObject.Find("RacingButton").GetComponentInChildren<Button>();
+		racing.onClick.AddListener(()=> switchScenes("2"));
+        
+        balancing = GameObject.Find("BalancingButton").GetComponentInChildren<Button>();
+		balancing.onClick.AddListener(()=> switchScenes("3")); 
+        
+        dodge = GameObject.Find("DodgeButton").GetComponentInChildren<Button>();
+		dodge.onClick.AddListener(()=> switchScenes("4"));
     }
-
-    void onClick(int game){
-
-    }
-
-    // void onFruit(){
-    //     // selectedGame =1;
-    //     switchScenes("1");
-    // }
-    // void onRacing(){
-    //     // selectedGame =2;
-    //     switchScenes("2");
-    // }
-    // void onBalancing(){
-    //     // selectedGame =3;
-    //     switchScenes("3");
-    // }
-    // void onDodge(){
-    //     // selectedGame =4;
-    //     switchScenes("4");
-    // }
 
     void switchScenes(string game){
-        // switch scene
-        SceneManager.LoadScene("HowToPlay");
+        // get chosen game and load scene
+        switch (game){
+            case "1": 
+                // Annabel: SAVE CHOSEN GAME
+                // switch scenes. Scene 0: HowToPlay
+                Debug.Log("Game selected: Catch the Fruit");
+                SceneManager.LoadScene(1);
+                break;
+            case "2":
+                Debug.Log("Game selected: Racing");
+                SceneManager.LoadScene(1);
+                break;
+            case "3":
+                Debug.Log("Game selected: Balancing");
+                SceneManager.LoadScene(1);
+                break;
+            case "4":
+                Debug.Log("Game selected: Dodge");
+                SceneManager.LoadScene(1);
+                break;
+        }
     }
-
-
 }
