85c59087035828ba6d31bd486a25444ba7ecc1c0
RuijieLi187 <60748594+RuijieLi187@users.noreply.github.com>
2020-02-20 15:09:01 +0000

    finished the game
    finished the game

---
+++ SME-G/catchfruit/Assets/Script/Quit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quit : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+                Debug.Log("exit");
+#else
+            Application.Quit();
+            Debug.Log ("游戏退出"):
+#endif
+            }
+        
+    }
+}
--- SME-G/catchfruit/Assets/Script/Timer.cs
+++ SME-G/catchfruit/Assets/Script/Timer.cs
@@ -8,31 +8,36 @@ using UnityEngine.SceneManagement;
 public class Timer : MonoBehaviour
 {
     public float timeStart = 60;
-    public Text TimerText; 
-    
-    void Awake(){
+    public Text TimerText;
+
+    void Awake()
+    {
         // find element in UI
         TimerText = GameObject.Find("TimerText").GetComponent<Text>();
         TimerText.text = "60";
     }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         TimerText.text = timeStart.ToString();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         timeStart -= Time.deltaTime;
         TimerText.text = Mathf.Round(timeStart).ToString();
 
-        if (timeStart <= 0){
+        if (timeStart <= 0)
+        {
             // restarts game 
             //CHANGE TO: displayEndOfGameMenu()
             StartCoroutine(ReStartGame());
         }
-	}
-    IEnumerator ReStartGame() {
+    }
+    IEnumerator ReStartGame()
+    {
         yield return new WaitForSecondsRealtime(0.5f);
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
