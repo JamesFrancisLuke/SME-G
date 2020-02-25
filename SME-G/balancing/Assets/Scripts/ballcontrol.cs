dae6fd8030a4aa11266a8cede9e1b0b14814daab
RuijieLi187 <60748594+RuijieLi187@users.noreply.github.com>
2020-02-25 11:22:16 +0000

    basic balancing game
    basic balancing game

---
+++ SME-G/balancing/Assets/Scripts/ballcontrol.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballcontrol : MonoBehaviour
+{
+    Transform cam;
+    Rigidbody rigid;
+    Vector3 force;
+  public float power;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main.transform;
+        rigid = this.GetComponent<Rigidbody>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+       // rigid.AddForce(Vector3.forward* 50, ForceMode.Force);
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 forward = (transform.position - cam.position);
+        forward.y = 0;
+
+        forward = forward.normalized;
+        Vector3 right = Quaternion.Euler(0, 90, 0) * forward;
+        force = forward * v + right * h;
+
+    
+
+    }
+    private void FixedUpdate()
+    {
+        rigid.AddForce(force*power);
+    }
+    
+}
