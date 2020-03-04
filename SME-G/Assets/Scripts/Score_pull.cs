03dc4a268e705fc63736bc96a50c86e8d2441cb4
Hau5ratz <16449790+Hau5ratz@users.noreply.github.com>
2020-03-04 15:24:07 +0000

    update with final scene
    good job folks

---
+++ SME-G/Assets/Scripts/Score_pull.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        getScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void getScore(){
+    	Text Score = GameObject.Find("Scoretxt").Getcomponent<Text>();
+    	string player_score = PlayerPrefs.GetString("playerScore");
+    	Debug.Log("Score=" + player_score);
+    	Score.text = player_score;
+    }
+
+}
